e5ac3382d6b5a3dac24a122a15774acc6a372a4f
Ander <ander.aranfdz@gmail.com>
2022-02-21 15:32:16 +0100

    Fix list-artifacts

--- list-artifacts.sh
+++ list-artifacts.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+REPO=$1
+
 curl -u aran159:$GITHUB_TOKEN \
     -H "Accept: application/vnd.github.v3+json" \
-    "https://api.github.com/repos/datua-digital/app-tutores-ue/actions/artifacts"
+    "https://api.github.com/repos/$1/actions/artifacts"
